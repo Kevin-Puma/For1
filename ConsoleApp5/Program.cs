b417a8cf30e43a02fcb7d14e03af6b35583ad726
Estudiantes <Estudiantes@ULELAB331>
2023-09-15 15:55:05 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,22 @@
+﻿    Console.Write("Ingrese un numero limite: ");
+    int limite =  int.Parse(Console.ReadLine());
+    int suma = 0;
+    Console.Write("Los numeros pares son: ");
+    for (int i = 1; i <= limite; i++)
+    {
+        if (i % 2==0) 
+        {
+            Console.Write(i+ ", ");
+            suma = suma + i;
+        } 
+    }
+    Console.WriteLine();
+    Console.WriteLine("La suma es:"+suma);
+    Console.ReadKey();    
+
+    
+
+
+
+
+    
